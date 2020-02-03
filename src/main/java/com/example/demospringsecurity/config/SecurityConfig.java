e4d7d256939f995418800cc964fbd3cd0126d9a2
sangyongchoi <csy7792@naver.com>
2020-02-04 02:54:32 +0900

    DB를 이용한 로그인구현

--- src/main/java/com/example/demospringsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/demospringsecurity/config/SecurityConfig.java
@@ -9,15 +9,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
-    protected void configure(HttpSecurity http) throws Exception{
+    protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests() // HttpServletRequest 을 사용하여 접근제한 설정 가능
-                .mvcMatchers("/","/info").permitAll()  // "/" , "/info" 요청은 모두 허용한다.
+                .mvcMatchers("/", "/info","/account/**").permitAll()  // "/" , "/info" 요청은 모두 허용한다.
                 .mvcMatchers("/admin").hasRole("ADMIN") // "/admin" 요청은 "ADMIN" 권한을 가지고 있어야 허용한다
                 .anyRequest().authenticated(); // 그 외의 요청은 권한이 있기만 하면 허용한다.
 
         http.formLogin(); // Security에서 기본적으로 제공하는 Login폼 사용
         http.httpBasic(); // HTTP 기본 인증을 구성
     }
-
-
 }
